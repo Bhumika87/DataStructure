203249cc8fe2fc2f0dad25234f33f60fbe00157d
bhumika <sbhumika959@gmail.com>
2025-12-16 02:52:40 +0530

    Array problem

--- ArraysAndHashing/containsDuplicates.java
+++ ArraysAndHashing/containsDuplicates.java
@@ -1,2 +1,49 @@
-package ArraysAndHashing;public class containsDuplicates {
+package ArraysAndHashing;
+
+import java.util.*;
+
+public class containsDuplicates {
+
+    //brute force
+    public boolean containsDuplicate1(int[] nums) {
+        Arrays.sort(nums);
+        for(int i =0;i<nums.length-1;i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //hashmap
+        public boolean containsDuplicate2(int[] nums) {
+            HashMap<Integer, Integer> hp = new HashMap<>();
+            for(int i =0;i<nums.length;i++){
+                if(hp.containsKey(nums[i])){
+                    return true;
+                }
+                hp.put(nums[i],1);
+            }
+            return false;
+        }
+    //hashset
+    public boolean containsDuplicate3(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i =0;i<nums.length;i++){
+            if(hs.contains(nums[i])){
+                return true;
+            }
+            hs.add(nums[i]);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        containsDuplicates cd = new containsDuplicates();
+        int[] nums = {1,2,3,1};
+        System.out.println(cd.containsDuplicate1(nums));
+        System.out.println(cd.containsDuplicate2(nums));
+        System.out.println(cd.containsDuplicate3(nums));
+    }
+
 }
--- String/palindrome.java
+++ String/palindrome.java
@@ -19,6 +19,28 @@ public class palindrome {
         return true;
     }
 
+    //125. Valid Palindrome
+    //Input: s = "A man, a plan, a canal: Panama"
+    //Output: true
+    //Explanation: "amanaplanacanalpanama" is a palindrome.
+    public boolean isPalindromeString(String s) {
+        int left =0;
+        int right = s.length()-1;
+        char head,tail;
+        while(left<right){
+            head = s.charAt(left);
+            tail = s.charAt(right);
+            if(!Character.isLetterOrDigit(head)) left++;
+            else if (!Character.isLetterOrDigit(tail)) right--;
+            else if(Character.toLowerCase(head)!=Character.toLowerCase(tail))return false;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String str = "abcdba";
         palindrome obj = new palindrome();
